0e5d2d11f0c168c1951f1603c5f9d5ef129eacdc
Randy Burnet <rrw.burnet@gmail.com>
2020-06-28 13:05:32 +0200

    props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
+class App extends Component {
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App Component</h1>
+      <PersonInfo name="Jermain" occupation="software engineer" />
+      <TotalScore points="3" />
     </div>
   );
+}
+}
+
+const PersonInfo = ({ name, occupation }) => {
+  return (
+    <div>
+      <h2>Display name: {name} </h2>
+      <h3>occupation: {occupation}</h3>
+    </div>
+  );
+};
+
+const TotalScore = (props) => {
+  return (
+    <div>
+      score: {props.points}
+    </div>
+  )
 }
 
 export default App;
